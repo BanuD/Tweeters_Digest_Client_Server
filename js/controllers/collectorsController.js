c6aa9291421c32f2304c3467fe717dd9002e47d5
Daniel Kelley <daniel.congdon.kelley@gmail.com>
2015-02-17 11:57:54 -0800

    changes params passed to post to /collectors to not be nested in another params hash

--- js/controllers/collectorsController.js
+++ js/controllers/collectorsController.js
@@ -1,14 +1,13 @@
 angular.module('app').controller('collectorsController', ["$http", 'leaders', '$cookieStore', function($http, leaders, $cookieStore) {
   var vm = this;
-  vm.leaders = leaders
+  vm.leaders = leaders;
+  vm.query = "";
   // vm.leaders = [{id: 1, handle: "@jennnnn"}, {id: 2, handle: "@danimalkelley"}]
-  vm.selected_leader = vm.leaders[0]
+  vm.selected_leader = vm.leaders[0];
   // vm.userCollectors = [{leader_id: 453, query: "some thoughts on cats"}, {leader_id: 333, query: "twitter stuff"}];
   vm.addCollector = function() {
-    var url = "http://localhost:3000/" + $cookieStore.get('current_user').id + "/collectors"
-    $http.post(url, {
-      params: { leader_id: vm.selected_leader, query: vm.query }
-    })
+    var url = "http://localhost:3000/users/" + $cookieStore.get('current_user').id + "/collectors"
+    $http.post(url, { leader_id: vm.selected_leader.id, query: vm.query })
     .then(function(data){
       vm.userCollectors.push(data)
       //To clear the entered values from the form
