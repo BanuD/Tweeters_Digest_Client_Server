5a44c4eed15bdcda50f7329c1b9d703acfa43e7a
Daniel Kelley <daniel.congdon.kelley@gmail.com>
2015-02-17 15:46:22 -0800

    fixes push statement for addGathering() to be response.data

--- js/controllers/gatheringsController.js
+++ js/controllers/gatheringsController.js
@@ -8,8 +8,8 @@ angular.module('app').controller('gatheringsController', ["$http", 'leaders', 'g
   vm.addGathering = function() {
     var url = "http://localhost:3000/users/" + $cookieStore.get('current_user').id + "/gatherings"
     $http.post(url, {leader_id: vm.selected_leader.id, query: vm.query })
-    .then(function(data){
-      vm.userGatherings.push(data)
+    .then(function(response){
+      vm.userGatherings.push(response.data.gathering)
       //To clear the entered values from the form
       // vm.leader = '';
       // vm.query = '';
