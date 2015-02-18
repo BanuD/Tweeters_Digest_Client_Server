ceff2fdb099c3404a5847035692cc936e87365af
Daniel Kelley <daniel.congdon.kelley@gmail.com>
2015-02-17 17:12:43 -0800

    starts to link create gathering field to the current content for that gathering for that user

--- js/controllers/gatheringsController.js
+++ js/controllers/gatheringsController.js
@@ -1,9 +1,39 @@
 angular.module('app').controller('gatheringsController', ["$http", 'leaders', 'gatherings', '$cookieStore', function($http, leaders, gatherings, $cookieStore) {
   var vm = this;
   vm.leaders = leaders.all_leaders;
-  vm.query = "";
   vm.selected_leader = vm.leaders[0]
   vm.userGatherings = gatherings.gatherings;
+
+  var myfunc = function(){
+    for(var i = 0; i < vm.userGatherings.length; i++){
+      if(vm.userGatherings[i].leader_id == vm.selected_leader.id ) {
+        return vm.userGatherings[i].query
+      }
+    }
+  }
+  // vm.leadersGatherings = function(){
+
+  vm.selected_query = myfunc();
+  // }
+
+  // vm.query = leadersGatherings()[vm.selected_leader.id]['query']
+  // vm.getLeaderQuery = function(leader){
+
+  // }
+
+  // vm.query = getLeaderQuery(vm.selected_leader);
+
+  vm.deleteGathering = function(gathering_id){
+    console.log('inside deleteGathering')
+    // var url = "http://localhost:3000/users/" + $cookieStore.get('current_user').id + "/gatherings"
+    // $http.delete(url)
+    // .then(function(response){
+    //   vm.userGatherings.remove(response.data.gathering)
+    //   //To clear the entered values from the form
+    //   // vm.leader = '';
+    //   // vm.query = '';
+    // })
+  }
 
   vm.addGathering = function() {
     var url = "http://localhost:3000/users/" + $cookieStore.get('current_user').id + "/gatherings"
